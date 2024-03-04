92a67db5c2d951f9eb2cc324ec05aa2da385979f
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-04 21:09:01 +0100

    working on the side bar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import "./Sidebar.css";
+
+export const Sidebar = () => {
+  return (
+    <div className="side-bar">
+      <div>LINK 1</div>
+      <div>LINK 2</div>
+      <div>LINK 3</div>
+    </div>
+  );
+};
