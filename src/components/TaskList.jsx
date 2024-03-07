b57802fcb84637a246d37ee255a6f5e4492c3f28
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-07 17:06:14 +0100

    working on TaskCard

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -11,31 +11,5 @@ export const TaskList = () => {
     setTasksState(tasksState.filter((task) => task.id !== id));
   };
 
-  return (
-    <div>
-      {tasksState.map((task) => {
-        return (
-          <div className="user-card">
-            <h3>{task.title}</h3>
-            <div>
-              {task.status === "Done"
-                ? `${task.status}  ✅ ` // if done green
-                : task.status === "To Do"
-                ? `${task.status} ⏳` // if to do   else red2
-                : `${task.status} 🔄`}{" "}
-              {/*  */}
-            </div>
-
-            <div>{task.assignee}</div>
-
-            <button onClick={() => deleteTask(task.id)}>delete</button>
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <TaskCard />;
 };
-
-// 1. find out a way to have 3 conditionals (now we only have 2)
-// status icons: To Do :⏳, In Progress: 🔄, Done: ✅
-// 2. priority icons: Low: 🟢, Medium: 🟠, High: 🔴
