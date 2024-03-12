b5bb1c10113f133680298c0f75576b2ab63d6bc3
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-12 22:52:04 +0100

    sidebar styling

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+import { MyButton } from "./MyButton";
 import "./Sidebar.css";
 
 export const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div>Link 1</div>
-      <div>Link 2</div>
-      <div>Link 3</div>
+      <Link to="/">HOME</Link>
+      <Link to="/about">ABOUT</Link>
+      <Link to="/item">ITEMS</Link>
     </div>
   );
 };
